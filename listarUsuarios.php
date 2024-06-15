b2a3a342260f4ed2ecb3d33049f83b5852f590fe
ronhen15 <78941585+ronhen15@users.noreply.github.com>
2024-06-14 21:28:29 -0600

    Add files via upload
    Actualizacion de listar usuarios

--- listarUsuarios.php
+++ listarUsuarios.php
@@ -6,6 +6,8 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 	<title>Listar Usuarios</title>
+
+
 </head>
 <body>
 	<div class="container">
@@ -42,6 +44,14 @@
 </nav>
 </div>
 <div class="container">
+		<div class="container my-2 bg-light text-center">
+
+ 		<?php 
+ 		include "eliminar_usuario.php";
+ 		include "config/conexion.php";
+ 		 ?>
+ 	</div>
+
 	<h1 class="text-center">Listado de Usuarios</h1>
 	<div class="container d-flex justify-content-between">
 	
@@ -57,25 +67,79 @@
 			<th>Usuario</th>
 			<th>Correo</th>
 			<th>Contraseña</th>
-			<th></th>
+			<th>Acciones</th>
 		</tr>
-		<tr>
-			
-		</tr>
+		
 	<?php
 
-	include("conexion.php");
+	
 	$sql = $connexion->query("select * from registro_usuario");
 	
 	while ($usuarios = $sql->fetch_object()){
 		
-		echo "<tr><td> $usuarios->Id </td><td> $usuarios->nombre </td><td> $usuarios->apellido </td><td> $usuarios->user </td><td> $usuarios->correo </td><td> $usuarios->clave </td><td> <a href='Editar.php'><i class='bi bi-pencil-square'></i></a><a href='eliminar.php'><i class='bi bi-trash'></i></a></td></tr>";
+		echo "<tr>
+				<td> $usuarios->Id </td>
+				<td> $usuarios->nombre </td>
+				<td> $usuarios->apellido </td>
+				<td> $usuarios->user </td>
+				<td> $usuarios->correo </td>
+				<td> $usuarios->clave </td>
+				<td> 
+					<a href='editar_usuario.php?id=$usuarios->Id' class='btn btn-outline-warning m-1' data-toggle='modal' data-target='#exampleModal'><i class='bi bi-pencil-square'></i></a>
+					<a href='listarUsuarios.php?id=$usuarios->Id' class='btn btn-outline-danger m-1'><i class='bi bi-trash'></i></a>
+				</td>
+		</tr>";
 
 
 		}
+
+ 		
+  
  	?>
+ 
  </table>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Actualizar Usuario</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <?php 
+
+		while ($datos=$sql->fetch_object()) {
+
+			?>
+
+		<form action="" method="POST">
+		Nombre: <input class="form-control" type="text" name="nombre" value="<?= $datos->nombre ?>"><br>
+		Apellido: <input class="form-control" type="text" name="apellido" value="<?= $datos->apellido ?>"><br>
+		Usuario: <input class="form-control" type="text" name="user" value="<?= $datos->user ?>"><br>
+		Correo:	<input class="form-control" type="email" name="correo" value="<?php $datos->correo ?>"><br>
+		Contraseña: <input class="form-control" type="password" name="clave" value="<?php $datos->clave ?>"><br>
+		
+		
+	</form>
+			<?php } ?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Actualizar</button>
+      </div>
+    </div>
+  </div>
+</div>
+ 	
+
  	</div>
+
 
  	<div class="container bg-body-tertiary">
  	<div class="text-center" >
